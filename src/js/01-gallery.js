a0a86c97242bee2eedad9828670a788dccd6084c
Sergii Vlasenko <98455988+nepiushiu1@users.noreply.github.com>
2022-08-25 23:39:34 +0300

    1 задание

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,32 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const newElements = galleryItems
+  .map(
+    value =>
+      `
+     
+  <a class="gallery__link" href="${value.original}">
+     <img
+       class="gallery__image"
+      src="${value.preview}"
+       alt="${value.description}"
+     />
+   </a>
+ 
+ `
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', newElements);
+
+new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
