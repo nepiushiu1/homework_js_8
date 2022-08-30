f0a6afc3e3b764a9da7239673b7251576b744b7f
Сергей Власенко <98455988+nepiushiu1@users.noreply.github.com>
2022-08-30 13:29:16 +0300

    3 задание

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,35 +1,32 @@
-// import throttle from 'lodash.throttle';
-
-// const form = document.querySelector('.feedback-form');
-// form.addEventListener('input', throttle(onFormData, 500));
-// form.addEventListener('submit', onSubmitForm);
-
-// const formData = {};
-
-// function onFormData(e) {
-//   formData[e.target.name] = e.target.value;
-//   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-// }
-
-// function onSubmitForm(e) {
-//   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
-//   e.preventDefault();
-//   e.currentTarget.reset();
-//   localStorage.removeItem('feedback-form-state');
-// }
-
-// (function dataFromLocalStorage() {
-//   const data = JSON.parse(localStorage.getItem('feedback-form-state'));
-//   const email = document.querySelector('.feedback-form input');
-//   const message = document.querySelector('.feedback-form textarea');
-//   if (data) {
-//     email.value = data.email;
-//     message.value = data.message;
-//   }
-// });
+import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-form.addEventListener('input',writingToLocalStorage)
-function writingToLocalStorage(params) {
-  
+form.addEventListener('input', throttle(onFormData, 500));
+
+const formData = {};
+function onFormData(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
+
+function storageСheck() {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const email = document.querySelector('input');
+  const message = document.querySelector('textarea');
+  if (data) {
+    email.value = data.email;
+    message.value = data.message;
+  }
+};
+storageСheck();
+
+form.addEventListener('submit', cleansingTheForm);
+function cleansingTheForm(event) {
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+
+
