d0d4ffc4c6666b348d3a1d57e697c9a2c2742a23
Сергей Власенко <98455988+nepiushiu1@users.noreply.github.com>
2022-09-01 23:48:07 +0300

    исправление 2 задания

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,7 +2,7 @@
 import Player from '@vimeo/player';
 
 import throttle from 'lodash.throttle';
-const iframe = document.querySelector('iframe');
+const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
 player.on('timeupdate', throttle(onPlay, 1000));
@@ -11,6 +11,18 @@ function onPlay({ seconds }) {
   localStorage.setItem('videoplayer-current-time', seconds);
 }
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-
-
+const currentTime = localStorage.getItem('videoplayer-current-time');
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
+ 
