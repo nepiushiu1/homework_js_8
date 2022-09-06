45df7c7377a076cd4327b822fe5a7ea2efe63e37
Сергей Власенко <98455988+nepiushiu1@users.noreply.github.com>
2022-09-06 13:59:48 +0300

    исправление дз

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,24 +4,34 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
   const input = document.querySelector('input');
   const texterea = document.querySelector('textarea');
-
-
+  let formData = {};
+  storageСheck();
 form.addEventListener('input', throttle(onFormData, 500));
 
-const formData = {};
+
 function onFormData(event) {
   formData[event.target.name] = event.target.value;
+  
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
-storageСheck();
+
 
 
 form.addEventListener('submit', cleansingTheForm);
+
+
 function cleansingTheForm(event) {
   event.preventDefault();
-  console.log(formData);
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+if(data){
+  console.log(data);
+}
+
+  // console.log(formData);
   event.currentTarget.reset();
   localStorage.removeItem('feedback-form-state');
+  formData={}
 }
 
 
@@ -29,6 +39,7 @@ function storageСheck() {
   const data = JSON.parse(localStorage.getItem('feedback-form-state'));
   
   if (data) {
+    // formData = JSON.parse(data);
     input.value = data.email||'';
     texterea.value = data.message||'';
   }
